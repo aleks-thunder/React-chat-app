9bc8e535bcc363eec3c0eec6bf73fd82db3c3fd4
aleks-thunder <aleks.thunder.m@gmail.com>
2023-03-20 11:33:20 +0200

    added storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "styled-components";
+
+import { ThemeContextProvider, useThemeContext } from "../src/context";
+import store from "../src/store/store";
+
+const ThemedApp = ({ children }) => {
+  const { theme } = useThemeContext();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>{children}</Provider>
+    </ThemeProvider>
+  );
+};
+
+const globalDecorator = StoryFn => {
+  return (
+    <BrowserRouter>
+      <ThemeContextProvider>
+        <ThemedApp>
+          <StoryFn />
+        </ThemedApp>
+      </ThemeContextProvider>
+    </BrowserRouter>
+  );
+};
+
+export const decorators = [globalDecorator];
